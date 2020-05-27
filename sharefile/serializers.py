3113d8b3d5ca3074771725ce082f3c346a4dcde5
coder-fish <970710580@qq.com>
2020-05-27 10:59:22 +0800

    与表对应

--- sharefile/serializers.py
+++ sharefile/serializers.py
@@ -1,51 +1,8 @@
 from rest_framework import serializers
-from api import models
+from sharefile import models
 
 
-class UserSerializer(serializers.ModelSerializer):
+class ShareSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.User
-        fields = '__all__'
-
-
-class ImgSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Img
-        fields = '__all__'
-
-
-class DocSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Doc
-        fields = '__all__'
-
-
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Note
-        fields = '__all__'
-
-
-class RadioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Radio
-        fields = '__all__'
-
-
-
-class TrashSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Trash
-        fields = '__all__'
-
-
-class CofferSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Coffer
-        fields = '__all__'
-
-
-class VideoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Video
+        model = models.Share
         fields = '__all__'
